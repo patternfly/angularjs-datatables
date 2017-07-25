7eaea560f862f7e1609e3503567886c89a97a512
David Taylor <dtaylor113@gmail.com>
2017-07-25 16:07:09 -0400

    Added dist files

--- dist/plugins/scroller/angular-datatables.scroller.js
+++ dist/plugins/scroller/angular-datatables.scroller.js
@@ -1,62 +1,62 @@
 /*!
- * angular-datatables - v0.5.6
- * https://github.com/l-lin/angular-datatables
+ * angularjs-datatables - v0.5.7
+ * https://github.com/dtaylor113/angularjs-datatables
  * License: MIT
  */
 if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports === exports) {
-    module.exports = 'datatables.scroller';
+   module.exports = 'datatables.scroller';
 }
-(function(window, document, $, angular) {
+(function (window, document, $, angular) {
 
-    'use strict';
+'use strict';
 
-    // See http://datatables.net/extensions/scroller/
-    angular.module('datatables.scroller', ['datatables'])
-        .config(dtScrollerConfig);
+// See http://datatables.net/extensions/scroller/
+angular.module('datatables.scroller', ['datatables'])
+    .config(dtScrollerConfig);
 
-    /* @ngInject */
-    function dtScrollerConfig($provide) {
-        $provide.decorator('DTOptionsBuilder', dtOptionsBuilderDecorator);
+/* @ngInject */
+function dtScrollerConfig($provide) {
+    $provide.decorator('DTOptionsBuilder', dtOptionsBuilderDecorator);
 
-        function dtOptionsBuilderDecorator($delegate) {
-            var newOptions = $delegate.newOptions;
-            var fromSource = $delegate.fromSource;
-            var fromFnPromise = $delegate.fromFnPromise;
+    function dtOptionsBuilderDecorator($delegate) {
+        var newOptions = $delegate.newOptions;
+        var fromSource = $delegate.fromSource;
+        var fromFnPromise = $delegate.fromFnPromise;
 
-            $delegate.newOptions = function() {
-                return _decorateOptions(newOptions);
-            };
-            $delegate.fromSource = function(ajax) {
-                return _decorateOptions(fromSource, ajax);
-            };
-            $delegate.fromFnPromise = function(fnPromise) {
-                return _decorateOptions(fromFnPromise, fnPromise);
-            };
+        $delegate.newOptions = function() {
+            return _decorateOptions(newOptions);
+        };
+        $delegate.fromSource = function(ajax) {
+            return _decorateOptions(fromSource, ajax);
+        };
+        $delegate.fromFnPromise = function(fnPromise) {
+            return _decorateOptions(fromFnPromise, fnPromise);
+        };
 
-            return $delegate;
+        return $delegate;
 
-            function _decorateOptions(fn, params) {
-                var options = fn(params);
-                options.withScroller = withScroller;
-                return options;
+        function _decorateOptions(fn, params) {
+            var options = fn(params);
+            options.withScroller = withScroller;
+            return options;
 
-                /**
-                 * Add scroller compatibility
-                 * @returns {DTOptions} the options
-                 */
-                function withScroller() {
-                    var scrollerSuffix = 'S';
-                    options.dom = options.dom ? options.dom : $.fn.dataTable.defaults.sDom;
-                    if (options.dom.indexOf(scrollerSuffix) === -1) {
-                        options.dom = options.dom + scrollerSuffix;
-                    }
-                    return options;
+            /**
+             * Add scroller compatibility
+             * @returns {DTOptions} the options
+             */
+            function withScroller() {
+                var scrollerSuffix = 'S';
+                options.dom = options.dom ? options.dom : $.fn.dataTable.defaults.sDom;
+                if (options.dom.indexOf(scrollerSuffix) === -1) {
+                    options.dom = options.dom + scrollerSuffix;
                 }
+                return options;
             }
         }
-        dtOptionsBuilderDecorator.$inject = ['$delegate'];
     }
-    dtScrollerConfig.$inject = ['$provide'];
+    dtOptionsBuilderDecorator.$inject = ['$delegate'];
+}
+dtScrollerConfig.$inject = ['$provide'];
 
 
 })(window, document, jQuery, angular);
